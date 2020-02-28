1171d7a90d35f9f31211901f6d506b3522468f46
Christopher Leja <christopher.leja@gmail.com>
2020-02-28 22:03:12 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -7,7 +7,8 @@ class Actor < ActiveRecord::Base
   end
   
   def list_roles
-    "#{self.characters.name} - #{self.shows.name}"
+    characters.collect do |characters|
+      "#{self.characters.name} - #{self.shows.name}"
   end
   
   
